d4152b02dd8970787e6a1636a11a0096dabc2f7a
Niccar95 <niccar.95@outlook.com>
2026-01-22 11:56:34 +0100

    setup pg adapter to link to neon database

--- auth.ts
+++ auth.ts
@@ -1,6 +1,16 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import PostgresAdapter from "@auth/pg-adapter";
+import { Pool } from "pg";
+import { authConfig } from "./auth.config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  adapter: PostgresAdapter(pool),
 });
