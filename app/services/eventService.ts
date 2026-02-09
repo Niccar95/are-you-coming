0dfa6634cabe3fc309f15d5245daeb9d46dad898
Niccar95 <niccar.95@outlook.com>
2026-02-09 19:35:56 +0100

    Added new event field

--- app/services/eventService.ts
+++ app/services/eventService.ts
@@ -4,17 +4,18 @@ import { Event } from "../lib/models/Event";
 export const getEvents = async (): Promise<Event[]> => {
   const { rows } = await pool.query("SELECT * FROM events ORDER BY id ASC");
   return rows.map(
-    (event: Event) => new Event(event.id, event.name, event.eventDate),
+    (event: Event) => new Event(event.id, event.name, event.eventDate, event.description),
   );
 };
 
 export const addEvent = async (
   name: string,
   eventDate: Date,
+  description: string,
 ): Promise<Event> => {
   const { rows } = await pool.query(
-    "INSERT INTO events (name, event_date) VALUES ($1, $2) RETURNING *",
-    [name, eventDate],
+    "INSERT INTO events (name, event_date, description) VALUES ($1, $2, $3) RETURNING *",
+    [name, eventDate, description],
   );
-  return new Event(rows[0].id, rows[0].name, rows[0].event_date);
+  return new Event(rows[0].id, rows[0].name, rows[0].event_date, rows[0].description);
 };
