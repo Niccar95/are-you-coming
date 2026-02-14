fc4c2dd300a483824f3ae9a2652a8a7849be40c7
Niccar95 <niccar.95@outlook.com>
2026-02-15 00:41:02 +0100

    added google auth changes

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,11 @@ import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig: NextAuthConfig = {
-  providers: [Google],
+  providers: [
+    Google({
+      checks: ["state"],
+    }),
+  ],
   session: { strategy: "jwt" },
   pages: {
     signIn: "/",
