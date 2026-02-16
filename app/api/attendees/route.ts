82ee3850c2ad474b6427b13daf900c722dc5f2f4
Niccar95 <niccar.95@outlook.com>
2026-02-16 20:04:29 +0100

    unlogged users can now submit attendance

--- app/api/attendees/route.ts
+++ app/api/attendees/route.ts
@@ -1,12 +1,7 @@
 import { addAttendee } from "@/app/services/attendeeService";
-import { auth } from "@/auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-  const session = await auth();
-  if (!session) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
   const { event_id, name, email } = await req.json();
 
   const attendee = await addAttendee(event_id, name, email);
