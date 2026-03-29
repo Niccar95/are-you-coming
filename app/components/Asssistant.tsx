413a14ad53fa88900f04182caa988f08ffd04131
Niccar95 <niccar.95@outlook.com>
2026-03-29 20:34:15 +0200

    styled chat window

--- app/components/Asssistant.tsx
+++ app/components/Asssistant.tsx
@@ -2,18 +2,25 @@
 
 import { FormEvent, useState } from "react";
 import { SendHorizonal } from "lucide-react";
+import Image from "next/image";
 import Spinner from "./Spinner";
 
 const Asssistant = () => {
   const [userPrompt, setUserPrompt] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
-  const [assistantResponse, setAssistantResponse] = useState<string>("");
 
-  const [visiblePrompt, setVisiblePrompt] = useState<string>("");
+  const [visiblePrompts, setVisiblePrompts] = useState<
+    { role: "user" | "assistant"; content: string }[]
+  >([]);
 
   const handlePrompt = async (e: FormEvent) => {
     e.preventDefault();
     setLoading(true);
+
+    setVisiblePrompts((prev) => [
+      ...prev,
+      { role: "user", content: userPrompt },
+    ]);
 
     try {
       const response = await fetch("api/assistant", {
@@ -30,9 +37,11 @@ const Asssistant = () => {
 
       const data = await response.json();
 
-      setAssistantResponse(data);
       setUserPrompt("");
-      setVisiblePrompt(userPrompt);
+      setVisiblePrompts((prev) => [
+        ...prev,
+        { role: "assistant", content: data },
+      ]);
     } catch (error) {
       console.error("Error prompting AI assistant:", error);
     } finally {
@@ -41,21 +50,36 @@ const Asssistant = () => {
   };
 
   return (
-    <div className="card flex flex-col justify-between h-[400px] w-[400px]">
-      {loading && <Spinner />}
-      <div className="self-end text-sm text-zinc-700 bg-zinc-100 rounded-lg p-3 w-[250px]">
-        {assistantResponse}
+    <div className="card flex flex-col h-[400px] md:w-[400px] pt-0!">
+      <div className="w-full h-fit p-1 flex items-center gap-2">
+        <Image
+          src="/AI-assistant.svg"
+          alt="AI Assistant"
+          width={40}
+          height={40}
+        />
+        <span className="font-semibold text-zinc-800">Aria AI</span>
+
+        {loading && <Spinner />}
       </div>
 
-      <div className="text-sm text-zinc-700 bg-zinc-50 rounded-lg p-3">
-        {visiblePrompt}
+      <div className="flex flex-col h-full rounded-t-lg border-[0.5px] border-zinc-200 bg-zinc-50 p-3 gap-2 overflow-auto">
+        {visiblePrompts.map((visiblePrompt, i) => (
+          <article
+            key={i}
+            className={`text-sm text-zinc-700 ${visiblePrompt.role === "user" ? "bg-violet-100 self-start" : "bg-zinc-100 self-end"} rounded-lg p-3`}
+          >
+            {visiblePrompt.content}
+          </article>
+        ))}
       </div>
 
-      <form onSubmit={handlePrompt} className="form-card">
+      <form onSubmit={handlePrompt} className="form-card rounded-t-none!">
         <input
           type="text"
           value={userPrompt}
           onChange={(e) => setUserPrompt(e.target.value)}
+          placeholder="What do you need help with?"
           className="form-input"
         ></input>
         <button
